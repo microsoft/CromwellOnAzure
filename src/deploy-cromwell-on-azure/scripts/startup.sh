49c452dc260d7f22b0ac182675f8de27781164e1
Ben Moskowitz <59071590+bemosk@users.noreply.github.com>
2020-02-19 17:36:42 -0800

    Blobfuse recovery (#23)
    * fix for startup.sh
    
    * fix for mount_containers.sh
    
    * better comment
    
    * applying recommended changes
    
    * adding diagnostic return code printouts
    
    * Checking if blobfuse process is running, since mountpoint does not immediatelly detect the failure. Removed repeating "already mounted" and "mount error code" messages from logs when there are no errors. Logging "umount error code" only if umount failed. Returning 0 instead of 32 if volume is already mounted. Removed duplicate blobfuse code. Added date/time to log messages.
    
    Co-authored-by: Tony Bendis <abendis@microsoft.com>

--- src/deploy-cromwell-on-azure/scripts/startup.sh
+++ src/deploy-cromwell-on-azure/scripts/startup.sh
@@ -10,23 +10,35 @@ touch $log_file
 exec 1>$log_file
 exec 2>&1
 
-echo "CromwellOnAzure startup log"
-echo
-echo "mount_containers.sh:"
-cd /cromwellazure
-# Exit code 32 = already mounted:
-./mount_containers.sh -a STORAGEACCOUNTNAME || [[ $? == 32 || $? == 0 ]]
-echo
-echo "Mounted blobfuse containers:"
-findmnt -t fuse
-echo
-echo "docker-compose pull:"
-docker-compose pull --ignore-pull-failures
-echo
-echo "docker-compose up:"
-docker-compose -f /cromwellazure/docker-compose.yml -f /cromwellazure/docker-compose.override.yml up -d
-echo
-echo "Startup complete"
+function write_log() {
+    # Prepend the parameter value with the current datetime, if passed
+    echo ${1+$(date --iso-8601=seconds) $1}
+}
 
-# keep the process alive so blobfuse mounts stay mounted
-while true; do sleep 10000; done
+write_log "CromwellOnAzure startup log"
+write_log
+
+write_log "mount_containers.sh:"
+cd /cromwellazure
+./mount_containers.sh -a STORAGEACCOUNTNAME
+write_log
+
+write_log "Mounted blobfuse containers:"
+findmnt -t fuse
+write_log
+
+write_log "docker-compose pull:"
+docker-compose pull --ignore-pull-failures
+write_log
+
+write_log "docker-compose up:"
+docker-compose -f /cromwellazure/docker-compose.yml -f /cromwellazure/docker-compose.override.yml up -d
+write_log
+
+write_log "Startup complete"
+
+# Keep the process running and call mount periodically to remount volumes that were dropped due to blobfuse crash
+while true; do  
+    sleep 30  
+    mount -a -t fuse || write_log "mount error code: $?" 
+done 
