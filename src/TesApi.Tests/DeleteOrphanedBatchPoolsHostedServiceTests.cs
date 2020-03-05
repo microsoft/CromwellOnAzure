3ed532d80a4a5c73d180b35c8ea868fa6fb65419
Tony Bendis <abendis@microsoft.com>
2020-03-05 12:29:14 -0800

    Treating NodeDiskFull as executor error. Requeuing active jobs where pool was not created. Deleting TES pools not referenced by jobs. Improved error logging. (#50)

---
+++ src/TesApi.Tests/DeleteOrphanedBatchPoolsHostedServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TesApi.Web;
+
+namespace TesApi.Tests
+{
+    [TestClass]
+    public class DeleteOrphanedBatchPoolsHostedServiceTests
+    {
+        [TestMethod]
+        public async Task DeleteOrphanedAutoPoolsServiceOnlyDeletesPoolsNotReferencedByJobs()
+        {
+            var activePoolIds = new List<string> { "1", "2", "3" };
+            var poolIdsReferencedByJobs = new List<string> { "3", "4" };
+
+            var azureProxy = new Mock<IAzureProxy>();
+            azureProxy.Setup(p => p.GetActivePoolIdsAsync(It.IsAny<string>(), It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>())).ReturnsAsync(activePoolIds);
+            azureProxy.Setup(p => p.GetPoolIdsReferencedByJobsAsync(It.IsAny<CancellationToken>())).ReturnsAsync(poolIdsReferencedByJobs);
+
+            var deleteCompletedBatchJobsHostedService = new DeleteOrphanedAutoPoolsHostedService(azureProxy.Object, new NullLogger<DeleteOrphanedAutoPoolsHostedService>());
+
+            await deleteCompletedBatchJobsHostedService.StartAsync(default);
+
+            azureProxy.Verify(i => i.GetActivePoolIdsAsync("TES_", TimeSpan.FromMinutes(30), It.IsAny<CancellationToken>()));
+            azureProxy.Verify(i => i.GetPoolIdsReferencedByJobsAsync(It.IsAny<CancellationToken>()));
+            azureProxy.Verify(i => i.DeleteBatchPoolAsync("1", It.IsAny<CancellationToken>()), Times.Once);
+            azureProxy.Verify(i => i.DeleteBatchPoolAsync("2", It.IsAny<CancellationToken>()), Times.Once);
+            azureProxy.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ src/TesApi.Web/TesTaskExtensions.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Linq;
+using TesApi.Models;
+
+namespace TesApi.Web
+{
+    /// <summary>
+    /// <see cref="TesTask"/> extensions
+    /// </summary>
+    public static class TesTaskExtensions
+    {
+        /// <summary>
+        /// Writes to <see cref="TesTask"/> system log.
+        /// </summary>
+        /// <param name="tesTask"><see cref="TesTask"/></param>
+        /// <param name="logEntries">List of strings to write to the log.</param>
+        public static void WriteToSystemLog(this TesTask tesTask, params string[] logEntries)
+        {
+            if (logEntries != null && logEntries.Any(e => !string.IsNullOrEmpty(e)))
+            {
+                tesTask.Logs = tesTask.Logs ?? new List<TesTaskLog>();
+                tesTask.Logs.Add(new TesTaskLog { SystemLogs = logEntries.ToList() });
+            }
+        }
+    }
+}
