bd14524eb88ff3987ea238fce3a786702177e7f2
Matt McLoughlin <mattmcl@microsoft.com>
2020-05-28 07:34:38 -0700

    add -y to apt install command

--- src/deploy-cromwell-on-azure/scripts/install-cromwellazure.sh
+++ src/deploy-cromwell-on-azure/scripts/install-cromwellazure.sh
@@ -4,7 +4,7 @@
 
 # Install Docker and Docker Compose
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
