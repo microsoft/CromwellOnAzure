314e66bb541e1c63673075e8fe19e612e94fb5a9
Tony Bendis <abendis@microsoft.com>
2020-07-23 21:59:04 -0700

    Updating Azure Management packages. Using Microsoft.Azure.Management.Batch because its fluent version is deprecated. Moved public structs from AzureProxy into seperate files.

---
+++ src/TesApi.Web/AzureBatchAccountQuotas.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace TesApi.Web
+{
+    public struct AzureBatchAccountQuotas
+    {
+        public int ActiveJobAndJobScheduleQuota { get; set; }
+        public int DedicatedCoreQuota { get; set; }
+        public int LowPriorityCoreQuota { get; set; }
+        public int PoolQuota { get; set; }
+    }
+}
