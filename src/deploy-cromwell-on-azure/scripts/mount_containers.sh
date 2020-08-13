68b015867372ff9893896d2c190a775c1aec7a06
Tony Bendis <abendis@microsoft.com>
2020-08-13 14:40:54 -0700

    Wait for the apt locks to get released before proceeding with the install. Added timestamps to mount_containers script output to see what steps are taking longer than expected. (#131)

--- src/deploy-cromwell-on-azure/scripts/mount_containers.sh
+++ src/deploy-cromwell-on-azure/scripts/mount_containers.sh
@@ -6,7 +6,7 @@ set -o errexit
 set -o nounset
 set -o errtrace
 
-trap 'echo "mount_containers failed with exit code $?"' ERR
+trap 'echo_with_ts "mount_containers failed with exit code $?"' ERR
 
 while getopts a: option
 do
@@ -15,11 +15,16 @@ do
   esac
 done
 
+function echo_with_ts() {
+    # Prepend the parameter value with the current datetime, if passed
+    echo ${1+$(date --iso-8601=seconds) $1}
+}
+
 get_list_of_containers_to_mount () {
   local -n result=$1
-  echo "Getting access token for $default_storage_account"
+  echo_with_ts "Getting access token for $default_storage_account"
   storage_token=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://$default_storage_account.blob.core.windows.net" | grep -Po '"access_token":"\K([^"]*)')
-  echo "Getting list of containers to mount from containers-to-mount file"
+  echo_with_ts "Getting list of containers to mount from containers-to-mount file"
   containers=$(curl -s -X GET "https://$default_storage_account.blob.core.windows.net/configuration/containers-to-mount" -H "Authorization: Bearer $storage_token" -H "x-ms-version: 2018-03-28" -d '' )
   containers=$(tr -d "[:blank:]" <<< "$containers")    # remove all spaces
   containers=$(grep "^[^#]" <<< "$containers")    # remove all comment lines
@@ -29,25 +34,25 @@ get_list_of_containers_to_mount () {
 get_accessible_storage_containers () {
   local -n result=$1
 
-  echo "Getting management access token"
+  echo_with_ts "Getting management access token"
   mgmt_token=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com" | grep -Po '"access_token":"\K([^"]*)')
-  echo "Getting storage access token"
+  echo_with_ts "Getting storage access token"
   storage_token=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://storage.azure.com" | grep -Po '"access_token":"\K([^"]*)')
-  echo "Getting list of accessible subscriptions"
+  echo_with_ts "Getting list of accessible subscriptions"
   subscription_ids=$(curl -s -X GET "https://management.azure.com/subscriptions/?api-version=2019-08-01" -H "Authorization: Bearer $mgmt_token" | grep -Po '"id":"/subscriptions/\K([^"]*)' )
 
   for subscription_id in $subscription_ids; do
     resource_filter="resourceType%20eq%20'Microsoft.Storage/storageAccounts'"
 
-    echo "Getting list of storage accounts accessible by this VM in subscription $subscription_id"
+    echo_with_ts "Getting list of storage accounts accessible by this VM in subscription $subscription_id"
     account_ids=$(curl -s -X GET "https://management.azure.com/subscriptions/$subscription_id/resources?%24filter=$resource_filter&api-version=2019-05-10" -H "Authorization: Bearer $mgmt_token" \
       | grep -Po "/subscriptions/[^\"]*/providers/Microsoft.Storage/storageAccounts/[^\"]*")
 
     for account_id in $account_ids; do
       account_name=$(grep -Po '/subscriptions/[^"]*/providers/Microsoft.Storage/storageAccounts/\K([^"]*)' <<< "$account_id")
-      echo "Getting access key for storage account $account_name"
+      echo_with_ts "Getting access key for storage account $account_name"
       account_key=$(curl -s -X POST "https://management.azure.com/$account_id/listKeys?api-version=2016-12-01" -H "Authorization: Bearer $mgmt_token" -d '' | grep -Po '"key1","value":"\K([^"]*)' )
-      echo "Getting list of containers for storage account $account_name"
+      echo_with_ts "Getting list of containers for storage account $account_name"
       container_names=$(curl -s -X GET "https://$account_name.blob.core.windows.net/?comp=list" -H "Authorization: Bearer $storage_token" -H "x-ms-version: 2018-03-28" -d '' | grep -Po '<Name>\K([^<]*)' )
 
       for container_name in $container_names; do
@@ -57,15 +62,16 @@ get_accessible_storage_containers () {
   done
 }
 
-echo $default_storage_account
 declare -a container_patterns_to_mount
 declare -A accessible_containers
 declare -A include_patterns
 declare -a exclude_patterns
 
+echo_with_ts "mount_containers starting"
+
 get_list_of_containers_to_mount container_patterns_to_mount
 
-echo "Containers to mount:"
+echo_with_ts "Containers to mount:"
 printf "%s\n" "${container_patterns_to_mount[@]-}" 
 
 container_patterns_to_mount=("${container_patterns_to_mount[@]/.blob.core.windows.net/}")
@@ -80,16 +86,16 @@ for pattern in "${container_patterns_to_mount[@]}"; do
 done
 
 echo ""
-echo "account/container patterns to include:"
+echo_with_ts "account/container patterns to include:"
 for x in "${!include_patterns[@]}"; do echo "$x -> ${include_patterns[$x]}"; done
 echo ""
-echo "account/container patterns to exclude:"
+echo_with_ts "account/container patterns to exclude:"
 printf "%s\n" "${exclude_patterns[@]-}" 
 
 get_accessible_storage_containers accessible_containers
 
 echo ""
-echo "Containers accessible to the VM:"
+echo_with_ts "Containers accessible to the VM:"
 for x in "${!accessible_containers[@]}"; do echo "$x"; done
 echo ""
 
@@ -113,7 +119,7 @@ for x in "${!accessible_containers[@]}"; do
     && { [[ $account_name == $default_storage_account && ( $container_name == "configuration" || $container_name == "cromwell-executions" || $container_name == "cromwell-workflow-logs" ) ]] && mount_path_in_docker_container="/$container_name" || mount_path_in_docker_container="/$account_name/$container_name" ; } \
     && mkdir -p $mount_path \
     && sudo echo "/usr/sbin/mount.blobfuse $mount_path fuse _netdev,account_name=$account_name,container_name=$container_name,account_key=$account_key" >> /etc/fstab \
-    && echo "Added entry in fstab for container $container_name from account $account_name with mount path $mount_path on host and $mount_path_in_docker_container in docker using the account key." \
+    && echo_with_ts "Added entry in fstab for container $container_name from account $account_name with mount path $mount_path on host and $mount_path_in_docker_container in docker using the account key." \
     && cromwell_volumes+="      - type: bind\n        source: $mount_path\n        target: $mount_path_in_docker_container\n" ;
 done
 
@@ -131,7 +137,7 @@ for x in "${!include_patterns[@]}"; do
     && mount_path_in_docker_container="/$account_name/$container_name" \
     && mkdir -p $mount_path \
     && sudo echo "/usr/sbin/mount.blobfuse $mount_path fuse _netdev,account_name=$account_name,container_name=$container_name,sas_token=$sas_token" >> /etc/fstab \
-    && echo "Added entry in fstab for container $container_name from account $account_name with mount path $mount_path on host and $mount_path_in_docker_container in docker using the SAS token." \
+    && echo_with_ts "Added entry in fstab for container $container_name from account $account_name with mount path $mount_path on host and $mount_path_in_docker_container in docker using the SAS token." \
     && tes_ext_storage_containers+="https://$account_name.blob.core.windows.net/$container_name$sas_token;" \
     && cromwell_volumes+="      - type: bind\n        source: $mount_path\n        target: $mount_path_in_docker_container\n" ;
 done
@@ -144,4 +150,4 @@ sudo umount -a -f -t fuse
 sudo rm -f /data/cromwellazure/mount.blobfuse.log
 sudo mount -av -t fuse
 
-echo "mount_containers completed successfully"
+echo_with_ts "mount_containers completed successfully"
