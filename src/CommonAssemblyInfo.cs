716b5f19734fd851e2f0b983c49553c431478465
Tony Bendis <abendis@microsoft.com>
2020-09-30 16:37:17 -0700

    Updated default docker tags for TES and TriggerService to point to latest public images, so locally built deployer can be used without having to specify TesImageName and TriggerServiceImageName options. Updated the CromwellOnAzureVersion to include just the major and minor parts as those are the only ones used by the update logic. Updated versions in CommonAssemblyInfo.cs to "0.0.0.0" to hint that those don't have to be updated manually. All of the above versions are replaced with four-part version during the official build. (#163)

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -6,5 +6,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
