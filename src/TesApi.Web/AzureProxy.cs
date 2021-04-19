f0dfdbdd898e90fea5147321a839b81c43541bd5
Tony Bendis <abendis@microsoft.com>
2021-04-18 21:24:38 -0700

    Upgraded to .NET 5.0. Updated all packages.

--- src/TesApi.Web/AzureProxy.cs
+++ src/TesApi.Web/AzureProxy.cs
@@ -729,11 +729,11 @@ namespace TesApi.Web
         /// Gets an authenticated Azure Client instance
         /// </summary>
         /// <returns>An authenticated Azure Client instance</returns>
-        private static async Task<Azure.IAuthenticated> GetAzureManagementClientAsync()
+        private static async Task<Microsoft.Azure.Management.Fluent.Azure.IAuthenticated> GetAzureManagementClientAsync()
         {
             var accessToken = await GetAzureAccessTokenAsync();
             var azureCredentials = new AzureCredentials(new TokenCredentials(accessToken), null, null, AzureEnvironment.AzureGlobalCloud);
-            var azureClient = Azure.Authenticate(azureCredentials);
+            var azureClient = Microsoft.Azure.Management.Fluent.Azure.Authenticate(azureCredentials);
 
             return azureClient;
         }
