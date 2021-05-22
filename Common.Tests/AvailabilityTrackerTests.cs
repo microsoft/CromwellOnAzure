7de0d4b137cb33ea53345b2152824aaff6b7b508
Matt McLoughlin <mattmcl@microsoft.com>
2021-05-22 12:23:30 -0700

    Add copyright notice

--- Common.Tests/AvailabilityTrackerTests.cs
+++ Common.Tests/AvailabilityTrackerTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
