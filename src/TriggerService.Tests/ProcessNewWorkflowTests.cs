652275e7032fb494f30a26d45ad6dd97feb63c90
Blair Murri <BMurri@users.noreply.github.com>
2021-10-04 17:32:24 -0700

    Fix bad GitHub merge from master

--- src/TriggerService.Tests/ProcessNewWorkflowTests.cs
+++ src/TriggerService.Tests/ProcessNewWorkflowTests.cs
@@ -141,7 +141,7 @@ namespace TriggerService.Tests
                 .Setup(az => az.DeleteBlobIfExistsAsync(It.IsAny<string>(), $"new/Sample.json"))
                 .Callback(() => deleted = true);
 
-            var cromwellOnAzureEnvironment = new CromwellOnAzureEnvironment(loggerFactory.Object, azureStorage.Object, cromwellApiClient.Object, repository.Object);
+            var cromwellOnAzureEnvironment = new CromwellOnAzureEnvironment(loggerFactory.Object, azureStorage.Object, cromwellApiClient.Object, repository.Object, Enumerable.Repeat(azureStorage.Object, 1));
 
             await cromwellOnAzureEnvironment.ProcessAndAbortWorkflowsAsync();
 
