890cbe5eb38cb46c1111ebed49bc15c768ed65c5
Matt McLoughlin <mattmcl@microsoft.com>
2022-01-14 20:12:18 -0800

    Update Xilinx start task script

--- src/TesApi.Web/Scripts/xilinx-start-task.sh
+++ src/TesApi.Web/Scripts/xilinx-start-task.sh
@@ -3,14 +3,17 @@
 # Licensed under the MIT License.
 
 echo "Start Task: Running, Checking if Docker is installed"
-if ! docker -v ; then
-  echo "Start Task: Installing Docker"
-  sudo apt update && \
-  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common && \
-  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-  sudo apt-key add - && \
-  sudo add-apt-repository -y "deb[arch = amd64] https://download.docker.com/linux/ubuntu focal stable" && \
-  sudo apt update && \
-  sudo apt install -y docker-ce
+
+if ! command -v docker -v &> /dev/null
+then
+    echo "Start Task: Installing Docker"
+    sudo apt update && \
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common && \
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+    sudo apt-key add - && \
+    sudo add-apt-repository -y "deb[arch = amd64] https://download.docker.com/linux/ubuntu focal stable" && \
+    sudo apt update && \
+    sudo apt install -y docker-ce
 fi
+
 echo "Start Task: Completed, Docker is currently installed"
