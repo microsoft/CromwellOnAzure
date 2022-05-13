0a643dea18d0856170e069a62f9ce6865eeb66dc
Tony Bendis <abendis@microsoft.com>
2022-05-12 17:16:19 -0700

    Improving logging when unlocking the cromwell_db database.

--- src/deploy-cromwell-on-azure/scripts/startup.sh
+++ src/deploy-cromwell-on-azure/scripts/startup.sh
@@ -113,14 +113,15 @@ kv["TesImageSha"]=$(docker inspect --format='{{range (.RepoDigests)}}{{.}}{{end}
 kv["TriggerServiceImageSha"]=$(docker inspect --format='{{range (.RepoDigests)}}{{.}}{{end}}' ${kv["TriggerServiceImageName"]})
 rm -f .env && for key in "${!kv[@]}"; do echo "$key=${kv[$key]}" >> .env; done
 
-write_log "Removing db lock if managed mysql"
 if [ -n "$mysql_server_name" ]; then
-    fully_qualified_server_name=$mysql_server_name.mysql.database.azure.com
+    fully_qualified_server_name="$mysql_server_name.mysql.database.azure.com"
     mysql_db_name=${kv["MySqlDatabaseName"]}
     mysql_user_login=${kv["MySqlUserLogin"]}
     mysql_user_password=${kv["MySqlUserPassword"]}
-    lockTableExists=$(mysql -u $mysql_user_login -p$mysql_user_password $mysql_db_name -h $fully_qualified_server_name -P 3306 -e "SELECT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = '$mysql_db_name' AND table_name = 'DATABASECHANGELOGLOCK') AS '' ")
-    [[ $lockTableExists -eq '1' ]] && mysql -u $mysql_user_login -p$mysql_user_password $mysql_db_name -h $fully_qualified_server_name -P 3306 -e "UPDATE $mysql_db_name.DATABASECHANGELOGLOCK SET LOCKED = 0, LOCKGRANTED = null, LOCKEDBY = null WHERE ID = 1" || :
+    write_log "Checking if database $mysql_db_name is locked"
+    lockTableExists=$(mysql -u "$mysql_user_login" -p"$mysql_user_password" -h "$fully_qualified_server_name" -P 3306 -e "SELECT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = '$mysql_db_name' AND table_name = 'DATABASECHANGELOGLOCK') AS '' ")
+    [[ "$lockTableExists" -eq "1" ]] && isLocked=$(mysql -u "$mysql_user_login" -p"$mysql_user_password" -h "$fully_qualified_server_name" -P 3306 -e "SELECT CONV(LOCKED, 2, 2) AS '' FROM $mysql_db_name.DATABASECHANGELOGLOCK WHERE ID = 1") || isLocked=0
+    [[ "$isLocked" -eq "1" ]] && write_log "Removing lock from $mysql_db_name" && mysql -u "$mysql_user_login" -p"$mysql_user_password" -h "$fully_qualified_server_name" -P 3306 -e "UPDATE $mysql_db_name.DATABASECHANGELOGLOCK SET LOCKED = 0, LOCKGRANTED = null, LOCKEDBY = null WHERE ID = 1" || write_log "$mysql_db_name was not locked"
 fi
 
 write_log "Running docker-compose up"
