d41d35640382e5ff5346a1ea589d437e8cdeef66
Matt McLoughlin <mattmcl@microsoft.com>
2022-07-25 21:25:13 -0700

    Update all projects to .NET 6 and all NuGet packages (#429)
    * Update to .NET 6
    
    * Update all projects to .NET 6, all NuGet packages, and fix warning for RNG

--- src/deploy-cromwell-on-azure/Utility.cs
+++ src/deploy-cromwell-on-azure/Utility.cs
@@ -108,9 +108,7 @@ namespace CromwellOnAzureDeployer
 
             while (true)
             {
-                using var rng = new RNGCryptoServiceProvider();
-                var buffer = new byte[length];
-                rng.GetBytes(buffer);
+                var buffer = RandomNumberGenerator.GetBytes(length);
 
                 var password = Convert.ToBase64String(buffer)
                     .Replace("+", "-")
