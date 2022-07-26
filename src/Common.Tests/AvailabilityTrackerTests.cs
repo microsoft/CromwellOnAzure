3a1cf04e608818098341af52ea7379ed0debbc2f
Matt McLoughlin <mattmcl@microsoft.com>
2022-07-26 09:57:51 -0700

    Remove unused usings

--- src/Common.Tests/AvailabilityTrackerTests.cs
+++ src/Common.Tests/AvailabilityTrackerTests.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
 
 namespace Common.Tests
 {
