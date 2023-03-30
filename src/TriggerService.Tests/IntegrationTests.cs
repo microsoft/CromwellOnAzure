94e1d73b2f28494510f8aaf1a7d796671ce994bf
Matt McLoughlin <mattmcl@microsoft.com>
2023-03-30 15:55:06 -0700

    minor refactoring

--- src/TriggerService.Tests/IntegrationTests.cs
+++ src/TriggerService.Tests/IntegrationTests.cs
@@ -20,20 +20,20 @@ namespace TriggerService.Tests
         {
             const string testStorageAccountName = "";
             const string workflowsContainerSasToken = "";
-            var n = DateTime.UtcNow;
-            var workflowFriendlyName = $"mutect2-{n.Year}-{n.Month}-{n.Day}-{n.Hour}-{n.Minute}";
+            const int countOfWorkflowsToRun = 100;
             const string triggerFile = "https://raw.githubusercontent.com/microsoft/gatk4-somatic-snvs-indels-azure/main-azure/mutect2.trigger.json";
             const string containerName = "workflows";
+
+            var n = DateTime.UtcNow;
+            var workflowFriendlyName = $"mutect2-{n.Year}-{n.Month}-{n.Day}-{n.Hour}-{n.Minute}";
             using var client = new HttpClient();
             var response = await client.GetAsync(triggerFile);
             var content = await response.Content.ReadAsStringAsync();
 
-            var max = 10;
-
-            for (var i = 1; i <= max; i++)
+            for (var i = 1; i <= countOfWorkflowsToRun; i++)
             {
-                var blobName = $"new/{workflowFriendlyName}-{i}-of-{max}.json";
-                var blobClient = new BlobServiceClient(new Uri($"https://{testStorageAccountName}.blob.core.windows.net/{containerName}/{blobName}?{workflowsContainerSasToken}"));
+                var blobName = $"new/{workflowFriendlyName}-{i}-of-{countOfWorkflowsToRun}.json";
+                var blobClient = new BlobServiceClient(new Uri($"https://{testStorageAccountName}.blob.core.windows.net/{containerName}/{blobName}?{workflowsContainerSasToken.TrimStart('?')}"));
                 var container = blobClient.GetBlobContainerClient(containerName);
                 await container.GetBlobClient(blobName).UploadAsync(BinaryData.FromString(content), true);
             }
