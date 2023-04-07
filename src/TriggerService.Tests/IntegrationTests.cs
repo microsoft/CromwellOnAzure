2de837c828d5752985cc95124fd705de3ea92498
Matt McLoughlin <mattmcl@microsoft.com>
2023-04-06 20:11:18 -0700

    Updates to integration test

--- src/TriggerService.Tests/IntegrationTests.cs
+++ src/TriggerService.Tests/IntegrationTests.cs
@@ -9,6 +9,9 @@ namespace TriggerService.Tests
     [TestClass]
     public class IntegrationTests
     {
+        private const string testStorageAccountName = "";
+        private const string workflowsContainerSasToken = "";
+
         /// <summary>
         /// To run this test, specify a testStorageAccountName, a workflowsContainerSasToken, and remove the [Ignore] attribute
         /// </summary>
@@ -18,21 +21,20 @@ namespace TriggerService.Tests
         [TestMethod]
         public async Task RunScaleTestWithMutect2Async()
         {
-            const string testStorageAccountName = "";
-            const string workflowsContainerSasToken = "";
             const int countOfWorkflowsToRun = 100;
             const string triggerFile = "https://raw.githubusercontent.com/microsoft/gatk4-somatic-snvs-indels-azure/main-azure/mutect2.trigger.json";
             const string containerName = "workflows";
 
             var n = DateTime.UtcNow;
-            var workflowFriendlyName = $"mutect2-{n.Year}-{n.Month}-{n.Day}-{n.Hour}-{n.Minute}";
+            var workflowFriendlyName = $"mutect2";
+            var date = $"{n.Year}-{n.Month}-{n.Day}-{n.Hour}-{n.Minute}";
             using var client = new HttpClient();
             var response = await client.GetAsync(triggerFile);
             var content = await response.Content.ReadAsStringAsync();
 
             for (var i = 1; i <= countOfWorkflowsToRun; i++)
             {
-                var blobName = $"new/{workflowFriendlyName}-{i}-of-{countOfWorkflowsToRun}.json";
+                var blobName = $"new/{workflowFriendlyName}-{i:D3}-of-{countOfWorkflowsToRun:D3}-{date}.json";
                 var blobClient = new BlobServiceClient(new Uri($"https://{testStorageAccountName}.blob.core.windows.net/{containerName}/{blobName}?{workflowsContainerSasToken.TrimStart('?')}"));
                 var container = blobClient.GetBlobContainerClient(containerName);
                 await container.GetBlobClient(blobName).UploadAsync(BinaryData.FromString(content), true);
