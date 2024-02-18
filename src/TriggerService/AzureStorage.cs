833e13410a7e3deaba0caef738da8f69fb636954
Jonathon Saunders <jsaun@users.noreply.github.com>
2024-02-17 22:30:50 -0800

    Add scope

--- src/TriggerService/AzureStorage.cs
+++ src/TriggerService/AzureStorage.cs
@@ -49,7 +49,7 @@ namespace TriggerService
             var azureClient = await GetAzureManagementClientAsync();
             var subscriptionIds = (await azureClient.Subscriptions.ListAsync()).Select(s => s.SubscriptionId);
 
-            var credentials = new TokenCredentials(await GetAzureAccessTokenAsync());
+            var credentials = new TokenCredentials(await AzureStorageUtility.GetAzureAccessTokenAsync());
 
             foreach (var subscriptionId in subscriptionIds)
             {
@@ -160,7 +160,7 @@ namespace TriggerService
         /// <returns>An authenticated Azure Client instance</returns>
         private static async Task<FluentAzure.IAuthenticated> GetAzureManagementClientAsync()
         {
-            var accessToken = await GetAzureAccessTokenAsync();
+            var accessToken = await AzureStorageUtility.GetAzureAccessTokenAsync();
             var azureCredentials = new AzureCredentials(new TokenCredentials(accessToken), null, null, AzureEnvironment.AzureGlobalCloud);
             var azureClient = FluentAzure.Authenticate(azureCredentials);
 
@@ -192,8 +192,5 @@ namespace TriggerService
 
             return blobList;
         }
-
-        private static async Task<string> GetAzureAccessTokenAsync()
-            => (await (new DefaultAzureCredential()).GetTokenAsync(new Azure.Core.TokenRequestContext())).Token;
     }
 }
