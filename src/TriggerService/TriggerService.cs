36aceb6d1d991bfbe9baa2089d23b4519f01846f
Matt McLoughlin <mattmcl@microsoft.com>
2024-03-03 12:45:08 -0800

    update trigger service cloud  config

--- src/TriggerService/TriggerService.cs
+++ src/TriggerService/TriggerService.cs
@@ -2,15 +2,21 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 using CommonUtilities.AzureCloud;
 using CromwellApiClient;
+using Microsoft.Azure.Management.ApplicationInsights.Management;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Rest;
 using Tes.Models;
 using Tes.Repository;
+using FluentAzure = Microsoft.Azure.Management.Fluent.Azure;
 
 namespace TriggerService
 {
@@ -20,39 +26,57 @@ namespace TriggerService
             => Common.NewtonsoftJsonSafeInit.SetDefaultSettings();
 
         public static async Task Main()
-            => await Host.CreateDefaultBuilder()
+        {
+            AzureCloudConfig azureCloudConfig = null;
+            string applicationInsightsConnectionString = "";
+
+            await Host.CreateDefaultBuilder()
                 .ConfigureAppConfiguration((hostBuilderContext, configurationBuilder) =>
                 {
                     configurationBuilder.AddJsonFile("appsettings.json");
                     configurationBuilder.AddEnvironmentVariables();
+                    var config = configurationBuilder.Build();
+                    azureCloudConfig = GetAzureCloudConfig(config);
+                    var triggerServiceOptions = new TriggerServiceOptions();
+                    hostBuilderContext.Configuration.GetSection(TriggerServiceOptions.TriggerServiceOptionsSectionName).Bind(triggerServiceOptions);
+
+                    if (!string.IsNullOrWhiteSpace(config["APPLICATIONINSIGHTS_CONNECTION_STRING"]))
+                    {
+                        // Legacy CoA setting
+                        Console.WriteLine("Using APPLICATIONINSIGHTS_CONNECTION_STRING");
+                        applicationInsightsConnectionString = config["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+                    }
+                    else if (!string.IsNullOrWhiteSpace(triggerServiceOptions.ApplicationInsightsAccountName))
+                    {
+                        Console.WriteLine("Getting Azure subscriptions and Application Insights Connection string...");
+                        // name was specified, get the subscription, then the connection string from the account
+                        var accessToken = new DefaultAzureCredential(new DefaultAzureCredentialOptions { AuthorityHost = new Uri(azureCloudConfig.Authentication.LoginEndpointUrl) }).GetTokenAsync(new Azure.Core.TokenRequestContext([azureCloudConfig.DefaultTokenScope])).Result.Token;
+                        var azureCredentials = new AzureCredentials(new TokenCredentials(accessToken), null, null, azureCloudConfig.AzureEnvironment);
+                        var azureManagementClient = FluentAzure.Authenticate(azureCredentials);
+                        var subscriptionId = azureManagementClient.Subscriptions.List().Select(s => s.SubscriptionId).First();
+                        var applicationInsightsManagementClient = new ApplicationInsightsManagementClient(azureCredentials) { SubscriptionId = subscriptionId, BaseUri = new Uri(azureCloudConfig.ResourceManagerUrl) };
+                        applicationInsightsConnectionString = applicationInsightsManagementClient
+                            .Components
+                            .List()
+                            .First(c => c.ApplicationId.Equals(triggerServiceOptions.ApplicationInsightsAccountName, StringComparison.OrdinalIgnoreCase))
+                            .ConnectionString;
+
+                        Console.WriteLine($"Successfully retrieved applicationInsightsConnectionString: {!string.IsNullOrWhiteSpace(applicationInsightsConnectionString)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No ApplicationInsights configuration found!");
+                    }
                 })
                 .ConfigureLogging(async (hostBuilderContext, loggingBuilder) =>
                     {
                         loggingBuilder.AddConsole();
-                        var triggerServiceOptions = new TriggerServiceOptions();
-                        hostBuilderContext.Configuration.GetSection(TriggerServiceOptions.TriggerServiceOptionsSectionName).Bind(triggerServiceOptions);
-
-                        if (string.IsNullOrWhiteSpace(triggerServiceOptions.ApplicationInsightsAccountName))
-                        {
-                            return;
-                        }
-
-                        Console.WriteLine($"ApplicationInsightsAccountName: {triggerServiceOptions.ApplicationInsightsAccountName}");
-                        var connectionString = await AzureStorage.GetAppInsightsConnectionStringAsync(triggerServiceOptions.ApplicationInsightsAccountName);
-
-                        if (string.IsNullOrWhiteSpace(connectionString))
-                        {
-                            throw new Exception($"No connection string found for {triggerServiceOptions.ApplicationInsightsAccountName}, does this service have Contributor access or equivalent to {triggerServiceOptions.ApplicationInsightsAccountName}?");
-                        }
-
                         loggingBuilder.AddApplicationInsights(
                             configuration =>
                             {
-                                configuration.ConnectionString = connectionString;
+                                configuration.ConnectionString = applicationInsightsConnectionString;
                             },
-                            options =>
-                            {
-                            });
+                            options => {});
 
                     })
                 .ConfigureServices((hostBuilderContext, serviceCollection) =>
@@ -60,7 +84,7 @@ namespace TriggerService
                     serviceCollection.Configure<CromwellApiClientOptions>(hostBuilderContext.Configuration.GetSection(CromwellApiClientOptions.CromwellApiClientOptionsSectionName));
                     serviceCollection.Configure<TriggerServiceOptions>(hostBuilderContext.Configuration.GetSection(TriggerServiceOptions.TriggerServiceOptionsSectionName));
                     serviceCollection.Configure<PostgreSqlOptions>(hostBuilderContext.Configuration.GetSection(PostgreSqlOptions.GetConfigurationSectionName("Tes")));
-                    serviceCollection.AddSingleton(AzureCloudConfig.CreateAsync().Result);
+                    serviceCollection.AddSingleton(azureCloudConfig);
                     serviceCollection.AddSingleton<ICromwellApiClient, CromwellApiClient.CromwellApiClient>();
                     serviceCollection.AddSingleton<IRepository<TesTask>, TesTaskPostgreSqlRepository>();
                     serviceCollection.AddSingleton<IAzureStorageUtility, AzureStorageUtility>();
@@ -68,5 +92,14 @@ namespace TriggerService
                 })
                 .Build()
                 .RunAsync();
+
+            static AzureCloudConfig GetAzureCloudConfig(IConfiguration configuration)
+            {
+                var options = new TriggerServiceOptions();
+                configuration.Bind(TriggerServiceOptions.TriggerServiceOptionsSectionName, options);
+                Console.WriteLine($"TriggerServiceOptions.AzureCloudName: {options.AzureCloudName}");
+                return AzureCloudConfig.CreateAsync(options.AzureCloudName, options.AzureCloudMetadataUrlApiVersion).Result;
+            }
+        }
     }
 }
--- src/TriggerService/TriggerServiceOptions.cs
+++ src/TriggerService/TriggerServiceOptions.cs
@@ -6,7 +6,15 @@ namespace TriggerService
     public class TriggerServiceOptions
     {
         public const string TriggerServiceOptionsSectionName = "TriggerService";
-        public string AzureCloudName { get; set; } = "AzureCloud";
+        /// <summary>
+        /// Azure cloud name.  Defined here: https://github.com/Azure/azure-sdk-for-net/blob/bc9f38eca0d8abbf0697dd3e3e75220553eeeafa/sdk/identity/Azure.Identity/src/AzureAuthorityHosts.cs#L11
+        /// </summary>
+        public string AzureCloudName { get; set; } = "AzureCloud"; // or "AzureUSGovernment"
+
+        /// <summary>
+        /// Version of the API to use to get the Azure cloud metadata
+        /// </summary>
+        public string AzureCloudMetadataUrlApiVersion { get; set; } = "2023-11-01";
         public string DefaultStorageAccountName { get; set; }
         public string ApplicationInsightsAccountName { get; set; }
         public int MainRunIntervalMilliseconds { get; set; } = 20000;
