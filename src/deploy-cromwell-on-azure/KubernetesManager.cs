65cefc46ac6456cec1b3ffa50803312728d1d36e
Matt McLoughlin <mattmcl@microsoft.com>
2024-03-05 10:16:10 -0800

    Update blobfuse version (#755)

--- src/deploy-cromwell-on-azure/KubernetesManager.cs
+++ src/deploy-cromwell-on-azure/KubernetesManager.cs
@@ -40,7 +40,7 @@ namespace CromwellOnAzureDeployer
 
         // "master" is used despite not being a best practice: https://github.com/kubernetes-sigs/blob-csi-driver/issues/783
         private const string BlobCsiDriverGithubReleaseBranch = "master";
-        private const string BlobCsiDriverGithubReleaseVersion = "v1.21.4";
+        private const string BlobCsiDriverGithubReleaseVersion = "v1.24.0";
         private const string BlobCsiRepo = $"https://raw.githubusercontent.com/kubernetes-sigs/blob-csi-driver/{BlobCsiDriverGithubReleaseBranch}/charts";
 
         private Configuration configuration { get; set; }
