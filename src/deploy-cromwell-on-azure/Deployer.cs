3cc61bcda1bea4c3b61b347eead785f1a321d559
Blair L Murri <BMurri@users.noreply.github.com>
2024-03-28 08:14:55 -0700

    Update schema for Service Info to match official GA4GH Service Info. (#686)

--- src/deploy-cromwell-on-azure/Deployer.cs
+++ src/deploy-cromwell-on-azure/Deployer.cs
@@ -323,10 +323,22 @@ namespace CromwellOnAzureDeployer
                             await kubernetesManager.RemovePodAadChart();
                         }
 
-                        if (installedVersion < new Version(5, 3, 0))
+                        if (installedVersion is null || installedVersion < new Version(5, 3, 0))
                         {
                             settings["AzureCloudName"] = configuration.AzureCloudName;
                         }
+
+                        if (installedVersion is null || installedVersion < new Version(5, 3, 1))
+                        {
+                            if (string.IsNullOrWhiteSpace(settings["DeploymentCreated"]))
+                            {
+                                settings["DeploymentCreated"] = settings["DeploymentUpdated"];
+                            }
+                        }
+
+                        //if (installedVersion is null || installedVersion < new Version(x, y, z))
+                        //{
+                        //}
 
                         if (waitForRoleAssignmentPropagation)
                         {
@@ -925,9 +937,11 @@ namespace CromwellOnAzureDeployer
         {
             settings ??= new();
             var defaults = GetDefaultValues(new[] { "env-00-coa-version.txt", "env-01-account-names.txt", "env-02-internal-images.txt", "env-03-external-images.txt", "env-04-settings.txt" });
+            var currentTime = DateTime.UtcNow;
 
             // We always overwrite the CoA version
             UpdateSetting(settings, defaults, "CromwellOnAzureVersion", default(string), ignoreDefaults: false);
+            UpdateSetting(settings, defaults, "DeploymentUpdated", currentTime.ToString("O"), ignoreDefaults: false);
 
             // Process images
             UpdateSetting(settings, defaults, "CromwellImageName", configuration.CromwellVersion, v => $"broadinstitute/cromwell:{v}",
@@ -939,6 +953,10 @@ namespace CromwellOnAzureDeployer
             // Additional non-personalized settings
             UpdateSetting(settings, defaults, "BatchNodesSubnetId", configuration.BatchNodesSubnetId);
             UpdateSetting(settings, defaults, "DisableBatchNodesPublicIpAddress", configuration.DisableBatchNodesPublicIpAddress, b => b.GetValueOrDefault().ToString(), configuration.DisableBatchNodesPublicIpAddress.GetValueOrDefault().ToString());
+            UpdateSetting(settings, defaults, "DeploymentOrganizationName", configuration.DeploymentOrganizationName);
+            UpdateSetting(settings, defaults, "DeploymentOrganizationUrl", configuration.DeploymentOrganizationUrl);
+            UpdateSetting(settings, defaults, "DeploymentContactUri", configuration.DeploymentContactUri);
+            UpdateSetting(settings, defaults, "DeploymentEnvironment", configuration.DeploymentEnvironment);
 
             if (installedVersion is null)
             {
@@ -963,6 +981,7 @@ namespace CromwellOnAzureDeployer
                 UpdateSetting(settings, defaults, "PostgreSqlCromwellDatabaseName", configuration.PostgreSqlCromwellDatabaseName, ignoreDefaults: true);
                 UpdateSetting(settings, defaults, "PostgreSqlCromwellDatabaseUserLogin", GetFormattedPostgresqlUser(isCromwellPostgresUser: true), ignoreDefaults: true);
                 UpdateSetting(settings, defaults, "PostgreSqlCromwellDatabaseUserPassword", configuration.PostgreSqlCromwellUserPassword, ignoreDefaults: true);
+                UpdateSetting(settings, defaults, "DeploymentCreated", currentTime.ToString("O"), ignoreDefaults: true);
             }
 
             BackFillSettings(settings, defaults);
@@ -2240,7 +2259,12 @@ namespace CromwellOnAzureDeployer
 
             if (!string.IsNullOrWhiteSpace(configuration.BatchNodesSubnetId) && !string.IsNullOrWhiteSpace(configuration.BatchSubnetName))
             {
-                throw new Exception("Invalid configuration options BatchNodesSubnetId and BatchSubnetName are mutually exclusive.");
+                throw new ValidationException("Invalid configuration options BatchNodesSubnetId and BatchSubnetName are mutually exclusive.");
+            }
+
+            if (string.IsNullOrWhiteSpace(configuration.DeploymentOrganizationName) != string.IsNullOrWhiteSpace(configuration.DeploymentOrganizationUrl))
+            {
+                throw new ValidationException("Invalid configuration options DeploymentOrganizationName and DeploymentOrganizationUrl must both be provided together.");
             }
         }
 
