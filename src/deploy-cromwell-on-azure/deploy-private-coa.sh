1ab1f6e94e5100f33c58be8e8afce1a14c0e9d13
Matt McLoughlin <mattmcl@microsoft.com>
2024-05-14 15:52:03 -0700

    add firewall

--- src/deploy-cromwell-on-azure/deploy-private-coa.sh
+++ src/deploy-cromwell-on-azure/deploy-private-coa.sh
@@ -3,14 +3,22 @@ set -e
 set -o pipefail
 
 # This script deploys a private instance of Cromwell on Azure (CoA) within a specified Azure subscription
-# and location. It creates a virtual network, then creates a VM jumpbox for deployment from within the virtual network, and executes
-# the CoA deployment binary. This script is designed to ensure a secure and isolated environment for running
-# Cromwell on Azure, suitable for sensitive or regulated workloads.
+# and location. It includes the setup of a virtual network, an Azure Firewall, and a VM jumpbox, ensuring
+# a secure environment for running Cromwell on Azure.
 
 # Usage: deploy-private-coa.sh <subscription> <location> <prefix> <azure_cloud_name>
 
 prefix="coa"
 azure_cloud_name="azurecloud"
+
+# Network configuration variables
+vnet_cidr="10.1.0.0/16"
+deployer_subnet_cidr="10.1.1.0/24"
+aks_subnet_cidr="10.1.2.0/24"
+psql_subnet_cidr="10.1.3.0/24"
+batch_subnet_cidr="10.1.128.0/17"
+kubernetes_service_cidr="10.1.4.0/24"
+kubernetes_dns_ip="10.1.4.10"
 
 # Check minimum required arguments
 if [ $# -lt 2 ]; then
@@ -30,6 +38,7 @@ vnet_name="${prefix}-vnet"
 deployer_subnet_name="${prefix}-deployer-subnet"
 nsg_name="${prefix}-aks-nsg"
 route_table_name="${prefix}-route-table"
+firewall_name="${prefix}-firewall"
 tes_image_name="mcr.microsoft.com/CromwellOnAzure/tes:5.3.1.12044"
 trigger_service_image_name="mcr.microsoft.com/CromwellOnAzure/triggerservice:5.3.1.12044"
 temp_deployer_vm_name="${prefix}-coa-deploy"
@@ -63,103 +72,50 @@ fi
 create_resource_group_if_not_exists $resource_group_name $location
 
 echo "Creating virtual network..."
-# Create the virtual network with the main address space
 az network vnet create \
     --resource-group $resource_group_name \
     --name $vnet_name \
-    --address-prefixes 10.1.0.0/16 \
+    --address-prefixes $vnet_cidr \
     --subnet-name $deployer_subnet_name \
-    --subnet-prefixes 10.1.1.0/24
+    --subnet-prefixes $deployer_subnet_cidr
+
+echo "Creating public IP for Azure Firewall..."
+az network public-ip create --name "${firewall_name}-pip" --resource-group $resource_group_name --location $location --sku "Standard" --allocation-method "Static"
+
+echo "Creating Azure Firewall..."
+az network firewall create --name $firewall_name --resource-group $resource_group_name --location $location --vnet-name $vnet_name
+
+echo "Creating firewall IP configuration..."
+az network firewall ip-config create --firewall-name $firewall_name --name "${firewall_name}-config" --public-ip-address "${firewall_name}-pip" --resource-group $resource_group_name --vnet-name $vnet_name
+
+firewall_private_ip=$(az network firewall show --name $firewall_name --resource-group $resource_group_name --query "ipConfigurations[0].privateIpAddress" --output tsv)
 
 echo "Creating route table..."
-az network route-table create \
-    --name $route_table_name \
-    --resource-group $resource_group_name \
-    --location $location
+az network route-table create --name $route_table_name --resource-group $resource_group_name --location $location
+
+echo "Creating route to direct AKS subnet Internet traffic through the Azure Firewall..."
+az network route-table route create --name "route-to-firewall" --route-table-name $route_table_name --resource-group $resource_group_name --address-prefix "0.0.0.0/0" --next-hop-type "VirtualAppliance" --next-hop-ip-address $firewall_private_ip
 
 echo "Creating Network Security Group for AKS..."
-az network nsg create \
-    --resource-group $resource_group_name \
-    --name $nsg_name \
-    --location $location
+az network nsg create --resource-group $resource_group_name --name $nsg_name --location $location
 
 echo "Setting NSG Rules for AKS..."
-# Block all inbound traffic from the internet
-az network nsg rule create \
-    --resource-group $resource_group_name \
-    --nsg-name $nsg_name \
-    --name block-internet-inbound \
-    --priority 100 \
-    --access Deny \
-    --direction Inbound \
-    --source-address-prefixes 'Internet' \
-    --destination-port-ranges '*' \
-    --protocol '*'
-
-# Allow all inbound traffic from within the virtual network
-az network nsg rule create \
-    --resource-group $resource_group_name \
-    --nsg-name $nsg_name \
-    --name allow-vnet-inbound \
-    --priority 110 \
-    --access Allow \
-    --direction Inbound \
-    --source-address-prefixes 'VirtualNetwork' \
-    --destination-port-ranges '*' \
-    --protocol '*'
-
-# Allow all outbound traffic
-az network nsg rule create \
-    --resource-group $resource_group_name \
-    --nsg-name $nsg_name \
-    --name allow-all-outbound \
-    --priority 120 \
-    --access Allow \
-    --direction Outbound \
-    --destination-port-ranges '*' \
-    --protocol '*' \
-    --destination-address-prefixes '*'
-
+az network nsg rule create --resource-group $resource_group_name --nsg-name $nsg_name --name "block-internet-inbound" --priority 100 --access Deny --direction Inbound --source-address-prefixes 'Internet' --destination-port-ranges '*' --protocol '*'
+az network nsg rule create --resource-group $resource_group_name --nsg-name $nsg_name --name "allow-vnet-inbound" --priority 110 --access Allow --direction Inbound --source-address-prefixes 'VirtualNetwork' --destination-port-ranges '*' --protocol '*'
+az network nsg rule create --resource-group $resource_group_name --nsg-name $nsg_name --name "allow-all-outbound" --priority 120 --access Allow --direction Outbound --destination-port-ranges '*' --protocol '*' --destination-address-prefixes '*'
 
 echo "Creating subnets..."
-
-deployer_subnet_id=$(az network vnet subnet show \
-    --resource-group $resource_group_name \
-    --vnet-name $vnet_name \
-    --name $deployer_subnet_name \
-    --query id -o tsv)
-
-# Create AKS subnet and associate it with the NSG
-az network vnet subnet create -g $resource_group_name --vnet-name $vnet_name -n aks-subnet --address-prefixes 10.1.2.0/24 --network-security-group $nsg_name --route-table $route_table_name
-az network vnet subnet create -g $resource_group_name --vnet-name $vnet_name -n psql-subnet --address-prefixes 10.1.3.0/24
-az network vnet subnet create -g $resource_group_name --vnet-name $vnet_name -n batch-subnet --address-prefixes 10.1.128.0/17
+az network vnet subnet create --resource-group $resource_group_name --vnet-name $vnet_name -n aks-subnet --address-prefixes $aks_subnet_cidr --network-security-group $nsg_name --route-table $route_table_name
+az network vnet subnet create --resource-group $resource_group_name --vnet-name $vnet_name -n psql-subnet --address-prefixes $psql_subnet_cidr
+az network vnet subnet create --resource-group $resource_group_name --vnet-name $vnet_name -n batch-subnet --address-prefixes $batch_subnet_cidr
 
 az network vnet subnet update --resource-group $resource_group_name --vnet-name $vnet_name --name aks-subnet --service-endpoints "Microsoft.Storage"
 az network vnet subnet update --resource-group $resource_group_name --vnet-name $vnet_name --name batch-subnet --service-endpoints "Microsoft.Storage"
-
-az network vnet subnet update \
-        --name psql-subnet \
-        --resource-group $resource_group_name \
-        --vnet-name $vnet_name \
-        --disable-private-endpoint-network-policies true
-
-az network vnet subnet update \
-        --name batch-subnet \
-        --resource-group $resource_group_name \
-        --vnet-name $vnet_name \
-        --disable-private-link-service-network-policies true
+az network vnet_subnet update --name psql-subnet --resource-group $resource_group_name --vnet-name $vnet_name --disable-private-endpoint-network-policies true
+az network vnet_subnet update --name batch-subnet --resource-group $resource_group_name --vnet-name $vnet_name --disable-private-link-service-network-policies true
 
 echo "Creating VM jumpbox within the virtual network to deploy from..."
-
-# Create the VM and specify the VNet's subnet by using the subnet ID
-vm_public_ip=$(az vm create \
-    --resource-group $resource_group_name \
-    --name $temp_deployer_vm_name \
-    --image Ubuntu2204 \
-    --admin-username azureuser \
-    --generate-ssh-keys \
-    --subnet $deployer_subnet_id \
-    --query publicIpAddress -o tsv)
+vm_public_ip=$(az vm create --resource-group $resource_group_name --name $temp_deployer_vm_name --image Ubuntu2204 --admin-username azureuser --generate-ssh-keys --subnet $deployer_subnet_id --query publicIpAddress -o tsv)
 
 echo "Opening port 22 for SSH access..."
 az vm open-port --port 22 --resource-group $resource_group_name --name $temp_deployer_vm_name
@@ -177,7 +133,7 @@ echo "Copying CoA deployment binary..."
 scp -o StrictHostKeyChecking=no $coa_binary azureuser@$vm_public_ip:/tmp/coa/$coa_binary
 
 echo "Installing Helm..."
-ssh -o StrictHostKeyChecking=no azureuser@${vm_public_ip} "curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+ssh -o StrictHostKeyChecking=no azureuser@$vm_public_ip "curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
 
 echo "Setting CoA deployer binary to executable..."
 ssh -o StrictHostKeyChecking=no azureuser@$vm_public_ip "chmod +x $coa_binary_path/$coa_binary"
@@ -200,5 +156,6 @@ ssh -o StrictHostKeyChecking=no azureuser@$vm_public_ip "$coa_binary_path/$coa_b
     --TesImageName $tes_image_name \
     --TriggerServiceImageName $trigger_service_image_name \
     --DebugLogging true \
-    --KubernetesServiceCidr 10.1.4.0/24 \
-    --KubernetesDnsServiceIp 10.1.4.10"
+    --KubernetesServiceCidr $kubernetes_service_cidr \
+    --KubernetesDnsServiceIp $kubernetes_dns_ip \
+    --UserDefinedRouting true"
