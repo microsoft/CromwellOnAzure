6aac59149b6d81cdf5f9437d068757f498c3b890
Matt McLoughlin <mattmcl@microsoft.com>
2024-05-17 13:42:47 -0700

    Create hub and spoke

--- src/deploy-cromwell-on-azure/deploy-private-coa.sh
+++ src/deploy-cromwell-on-azure/deploy-private-coa.sh
@@ -26,17 +26,6 @@ fi
 prefix="coa"
 azure_cloud_name="azurecloud"
 
-# Network configuration variables
-vnet_cidr="10.1.0.0/16"
-deployer_subnet_cidr="10.1.0.0/24"
-firewall_subnet_cidr="10.1.1.0/24"
-aks_subnet_cidr="10.1.2.0/24"
-psql_subnet_cidr="10.1.3.0/24"
-kubernetes_service_cidr="10.1.4.0/24"
-kubernetes_dns_ip="10.1.4.10"
-batch_subnet_cidr="10.1.128.0/17"
-
-# Check minimum required arguments
 if [ $# -lt 2 ]; then
     echo "Usage: $0 <subscription> <location> [prefix] [azure_cloud_name]"
     echo "Note: [prefix] defaults to 'coa' and [azure_cloud_name] defaults to 'azurecloud' if not provided."
@@ -48,11 +37,26 @@ location=$2
 prefix=${3:-$prefix}
 azure_cloud_name=${4:-$azure_cloud_name}
 
-# Resource Naming
+### VARIABLES
+# Network
+hub_vnet_cidr="10.0.0.0/16"
+hub_subnet_cidr="10.0.1.0/24"
+spoke0_vnet_cidr="10.100.0.0/16"
+spoke0_subnet_cidr="10.100.0.0/24"
+#firewall_subnet_cidr="10.100.1.0/24"
+aks_subnet_cidr="10.100.1.0/24"
+psql_subnet_cidr="10.100.2.0/24"
+kubernetes_service_cidr="10.100.3.0/24"
+kubernetes_dns_ip="10.100.3.10"
+batch_subnet_cidr="10.100.128.0/17"
+# Resource names
 resource_group_name="${prefix}-main"
-vnet_name="${prefix}-vnet"
+hub_vnet_name="${prefix}-hub-vnet"
+hub_subnet_name="AzureFirewallSubnet" # firewall will be placed in hub subnet
+spoke0_vnet_name="${prefix}-spoke0-vnet"
+spoke0_subnet_name="${prefix}-spoke-subnet"
 deployer_subnet_name="${prefix}-deployer-subnet"
-firewall_subnet_name="AzureFirewallSubnet" # "${prefix}-firewall-subnet"
+#firewall_subnet_name="AzureFirewallSubnet" # "${prefix}-firewall-subnet"
 route_table_name="${prefix}-route-table"
 firewall_name="${prefix}-firewall"
 dns_zone_name="${prefix}.private.${location}.azmk8s.io"
@@ -88,32 +92,43 @@ fi
 
 create_resource_group_if_not_exists $resource_group_name $location
 
-echo "Creating virtual network..."
+echo "Creating Hub virtual network..."
 az network vnet create \
     --resource-group $resource_group_name \
-    --name $vnet_name \
-    --address-prefixes $vnet_cidr \
-    --subnet-name $deployer_subnet_name \
-    --subnet-prefixes $deployer_subnet_cidr
+    --name $hub_vnet_name \
+    --address-prefixes $hub_vnet_cidr \
+    --subnet-name $hub_subnet_name \
+    --subnet-prefixes $hub_subnet_cidr
+ 
+echo "Creating spoke0 virtual network..."
 
-# DNS Zone setup
+az network vnet create \
+    --resource-group $resource_group_name \
+    --name $spoke0_vnet_name \
+    --address-prefixes $spoke0_vnet_cidr \
+    --subnet-name $spoke0_subnet_name \
+    --subnet-prefixes $spoke0_subnet_cidr
+
+az network vnet peering create --name HubToSpoke0 --resource-group $resource_group_name --vnet-name $hub_vnet_name --remote-vnet  $spoke_vnet_name --allow-vnet-access
+az network vnet peering create --name Spoke0ToHub --resource-group $resource_group_name --vnet-name $spoke_vnet_name --remote-vnet $hub_vnet_name --allow-vnet-access
+
 echo "Creating AKS private DNS zone..."
 dns_zone_id=$(az network private-dns zone create --resource-group $resource_group_name --name $dns_zone_name --query "id" -o tsv)
-az network private-dns link vnet create --resource-group $resource_group_name --zone-name $dns_zone_name --name "${vnet_name}-dns-link" --virtual-network $vnet_name --registration-enabled false
+az network private-dns link vnet create --resource-group $resource_group_name --zone-name $dns_zone_name --name "${spoke_vnet_name}-dns-link" --virtual-network $spoke_vnet_name --registration-enabled false
 
-echo "Creating firewall subnet..."
-az network vnet subnet create --resource-group $resource_group_name --vnet-name $vnet_name --name $firewall_subnet_name --address-prefixes $firewall_subnet_cidr
+#echo "Creating firewall subnet..."
+#az network vnet subnet create --resource-group $resource_group_name --vnet-name $vnet_name --name $firewall_subnet_name --address-prefixes $firewall_subnet_cidr
 
 echo "Creating public IP for Azure Firewall..."
 az network public-ip create --name "${firewall_name}-pip" --resource-group $resource_group_name --location $location --sku "Standard" --allocation-method "Static"
 
 echo "Creating Azure Firewall..."
-az network firewall create --name $firewall_name --resource-group $resource_group_name --location $location --vnet-name $vnet_name
+az network firewall create --name $firewall_name --resource-group $resource_group_name --location $location --vnet-name $hub_vnet_name
 
 firewall_public_ip_id=$(az network public-ip show --name "${firewall_name}-pip" --resource-group $resource_group_name --query "id" -o tsv)
 
 echo "Started at $(date '+%I:%M:%S%p'): Creating firewall IP configuration (takes 10-30 minutes)..."
-az network firewall ip-config create --firewall-name $firewall_name --name "${firewall_name}-config" --public-ip-address $firewall_public_ip_id --resource-group $resource_group_name --vnet-name $vnet_name --subnet $firewall_subnet_name
+az network firewall ip-config create --firewall-name $firewall_name --name "${firewall_name}-config" --public-ip-address $firewall_public_ip_id --resource-group $resource_group_name --vnet-name $hub_vnet_name --subnet $hub_subnet_name
 firewall_private_ip=$(az network firewall show --name $firewall_name --resource-group $resource_group_name | jq -r '.ipConfigurations[0].privateIPAddress')
 
 echo "Creating route table..."
@@ -123,16 +138,16 @@ echo "Creating route to direct AKS and Batch subnet Internet traffic through the
 az network route-table route create --name "route-to-firewall" --route-table-name $route_table_name --resource-group $resource_group_name --address-prefix "0.0.0.0/0" --next-hop-type "VirtualAppliance" --next-hop-ip-address $firewall_private_ip
 
 echo "Creating subnets..."
-az network vnet subnet create --resource-group $resource_group_name --vnet-name $vnet_name -n aks-subnet --address-prefixes $aks_subnet_cidr --route-table $route_table_name
-az network vnet subnet create --resource-group $resource_group_name --vnet-name $vnet_name -n psql-subnet --address-prefixes $psql_subnet_cidr
-az network vnet subnet create --resource-group $resource_group_name --vnet-name $vnet_name -n batch-subnet --address-prefixes $batch_subnet_cidr --route-table $route_table_name
+az network vnet subnet create --resource-group $resource_group_name --vnet-name $spoke0_vnet_name -n aks-subnet --address-prefixes $aks_subnet_cidr --route-table $route_table_name
+az network vnet subnet create --resource-group $resource_group_name --vnet-name $spoke0_vnet_name -n psql-subnet --address-prefixes $psql_subnet_cidr
+az network vnet subnet create --resource-group $resource_group_name --vnet-name $spoke0_vnet_name -n batch-subnet --address-prefixes $batch_subnet_cidr --route-table $route_table_name
 
-az network vnet subnet update --resource-group $resource_group_name --vnet-name $vnet_name --name aks-subnet --service-endpoints "Microsoft.Storage"
-az network vnet subnet update --resource-group $resource_group_name --vnet-name $vnet_name --name batch-subnet --service-endpoints "Microsoft.Storage"
-az network vnet subnet update --name psql-subnet --resource-group $resource_group_name --vnet-name $vnet_name --disable-private-endpoint-network-policies true
-az network vnet subnet update --name batch-subnet --resource-group $resource_group_name --vnet-name $vnet_name --disable-private-link-service-network-policies true
+az network vnet subnet update --resource-group $resource_group_name --vnet-name $spoke0_vnet_name --name aks-subnet --service-endpoints "Microsoft.Storage"
+az network vnet subnet update --resource-group $resource_group_name --vnet-name $spoke0_vnet_name --name batch-subnet --service-endpoints "Microsoft.Storage"
+az network vnet subnet update --name psql-subnet --resource-group $resource_group_name --vnet-name $spoke0_vnet_name --disable-private-endpoint-network-policies true
+az network vnet subnet update --name batch-subnet --resource-group $resource_group_name --vnet-name $spoke0_vnet_name --disable-private-link-service-network-policies true
 
-deployer_subnet_id=$(az network vnet subnet show --resource-group $resource_group_name --vnet-name $vnet_name --name $deployer_subnet_name --query "id" -o tsv)
+deployer_subnet_id=$(az network vnet subnet show --resource-group $resource_group_name --vnet-name $spoke0_vnet_name --name $deployer_subnet_name --query "id" -o tsv)
 
 echo "Creating VM jumpbox within the virtual network to deploy from..."
 vm_public_ip=$(az vm create --resource-group $resource_group_name --name $temp_deployer_vm_name --image Ubuntu2204 --admin-username azureuser --generate-ssh-keys --subnet $deployer_subnet_id --query publicIpAddress -o tsv)
@@ -167,7 +182,7 @@ ssh -o StrictHostKeyChecking=no azureuser@$vm_public_ip "$coa_binary_path/$coa_b
     --MainIdentifierPrefix $prefix \
     --PrivateNetworking true \
     --DisableBatchNodesPublicIpAddress true \
-    --VnetName $vnet_name \
+    --VnetName $spoke_vnet_name \
     --VnetResourceGroupName $resource_group_name \
     --VmSubnetName aks-subnet \
     --PostgreSqlSubnetName psql-subnet \
