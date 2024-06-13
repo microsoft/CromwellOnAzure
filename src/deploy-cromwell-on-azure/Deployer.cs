4a36358f18d4be637e01c0731fbe895297296a62
Blair L Murri <BMurri@users.noreply.github.com>
2024-06-12 17:12:55 -0700

    Handle OperationNotAllowed during upgrade

--- src/deploy-cromwell-on-azure/Deployer.cs
+++ src/deploy-cromwell-on-azure/Deployer.cs
@@ -69,8 +69,15 @@ namespace CromwellOnAzureDeployer
     public class Deployer
     {
         private static readonly AsyncRetryPolicy roleAssignmentHashConflictRetryPolicy = Policy
-            .Handle<Microsoft.Rest.Azure.CloudException>(cloudException => cloudException.Body.Code.Equals("HashConflictOnDifferentRoleAssignmentIds"))
+            .Handle<Microsoft.Rest.Azure.CloudException>(cloudException =>
+                "HashConflictOnDifferentRoleAssignmentIds".Equals(cloudException.Body.Code))
             .RetryAsync();
+
+        private static readonly AsyncRetryPolicy operationNotAllowedConflictRetryPolicy = Policy
+            .Handle<Azure.RequestFailedException>(azureException =>
+                (int)System.Net.HttpStatusCode.Conflict == azureException.Status &&
+                "OperationNotAllowed".Equals(azureException.ErrorCode))
+            .WaitAndRetryAsync(3, retryAttempt => System.TimeSpan.FromSeconds(10));
 
         private static readonly AsyncRetryPolicy generalRetryPolicy = Policy
             .Handle<Exception>()
@@ -318,7 +325,7 @@ namespace CromwellOnAzureDeployer
 
                         if (installedVersion is null || installedVersion < new Version(5, 2, 2))
                         {
-                            await EnableWorkloadIdentity(aksCluster, managedIdentity, resourceGroup);
+                            await operationNotAllowedConflictRetryPolicy.ExecuteAsync(() => EnableWorkloadIdentity(aksCluster, managedIdentity, resourceGroup));
                             await kubernetesManager.RemovePodAadChart();
                             await Execute("Waiting 2 minutes for federated crendentials propagation...",
                                 () => Task.Delay(System.TimeSpan.FromMinutes(2), cts.Token));
