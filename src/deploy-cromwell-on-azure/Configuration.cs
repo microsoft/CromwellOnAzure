e49f45eafecbb748ae7c3c5d4795a9008ca85668
Blair L Murri <BMurri@users.noreply.github.com>
2024-09-23 11:32:38 -0700

    MCR's docker mirror for ubuntu does not include "latest"

--- src/deploy-cromwell-on-azure/Configuration.cs
+++ src/deploy-cromwell-on-azure/Configuration.cs
@@ -96,7 +96,7 @@ namespace CromwellOnAzureDeployer
         public string DeploymentContactUri { get; set; }
         public string DeploymentEnvironment { get; set; }
         public string PrivateTestUbuntuImage { get; set; } = "mcr.microsoft.com/mirror/docker/library/ubuntu:22.04";
-        public string PrivatePSQLUbuntuImage { get; set; } = "mcr.microsoft.com/mirror/docker/library/ubuntu:latest";
+        public string PrivatePSQLUbuntuImage { get; set; } = "mcr.microsoft.com/mirror/docker/library/ubuntu:24.04"; // mcr's docker mirror does not host "latest"
 
         public static Configuration BuildConfiguration(string[] args)
         {
