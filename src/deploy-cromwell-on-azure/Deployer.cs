bccb45b87bb9db4a856e3836cb607b9214dc6ce1
Blair L Murri <BMurri@users.noreply.github.com>
2024-10-28 17:15:45 -0700

    fix the fix

--- src/deploy-cromwell-on-azure/Deployer.cs
+++ src/deploy-cromwell-on-azure/Deployer.cs
@@ -1369,7 +1369,7 @@ namespace CromwellOnAzureDeployer
             _ = ConsoleEx.WriteLine($"AadGroupIds: {configuration.AadGroupIds ?? "<null>"}", ConsoleColor.Yellow);
             _ = ConsoleEx.WriteLine($"AdminGroupObjectIds: {string.Join(", ", managedCluster.Data.AadProfile?.AdminGroupObjectIds ?? []):D}", ConsoleColor.Yellow);
             var adminGroupObjectIds = managedCluster.Data.AadProfile?.AdminGroupObjectIds ?? [];
-            adminGroupObjectIds = adminGroupObjectIds.Count == 0 ? adminGroupObjectIds : (string.IsNullOrWhiteSpace(configuration.AadGroupIds) ? [] : configuration.AadGroupIds.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(Guid.Parse).ToList());
+            adminGroupObjectIds = adminGroupObjectIds.Count != 0 ? adminGroupObjectIds : (string.IsNullOrWhiteSpace(configuration.AadGroupIds) ? [] : configuration.AadGroupIds.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(Guid.Parse).ToList());
             _ = ConsoleEx.WriteLine($"adminGroupObjectIds: {string.Join(", ", adminGroupObjectIds):D}", ConsoleColor.Yellow);
 
             if (adminGroupObjectIds.Count == 0)
